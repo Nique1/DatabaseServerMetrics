4828bad7c186324dc6e6fdebb27be1c79f0653e1
Nique1 <dominikasarata@gmail.com>
2023-11-04 16:42:54 +0100

    delete procedure operations classes

--- src/package1/Main.java
+++ src/package1/Main.java
@@ -11,7 +11,6 @@ public class Main {
 
         ResponseTimeMeasure responseTimeMeasure = new ResponseTimeMeasure();
 
-        ProcedureExecutor procedureExecutor = new ProcedureExecutor(dataSourceSwitcher.getDataSource1Connection());
 
         // Execute a query on the first data source
         ResultSet resultSet1 = queryExecutor.executeQueryOnDataSource1("WAITFOR DELAY '00:00:15'; SELECT * FROM Customers");
@@ -20,7 +19,7 @@ public class Main {
         //execute procedure
 
 
-        procedureExecutor.executeProcedure();
+
         // Execute a query on the second data source
 //        ResultSet resultSet2 = databaseOperations.executeQueryOnDataSource2("SELECT * FROM table2");
 
