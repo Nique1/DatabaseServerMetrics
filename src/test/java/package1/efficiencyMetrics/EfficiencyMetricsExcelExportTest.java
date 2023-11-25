002c1605a1da8f5e788ffd30621ead0e64dc13e7
Nique1 <dominikasarata@gmail.com>
2023-11-25 14:57:51 +0100

    Created unit testing environment

---
+++ src/test/java/package1/efficiencyMetrics/EfficiencyMetricsExcelExportTest.java
@@ -0,0 +1,10 @@
+package package1.efficiencyMetrics;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+class EfficiencyMetricsExcelExportTest {
+    void TestClass(){
+
+    }
+
+}
