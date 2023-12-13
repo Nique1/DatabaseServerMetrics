9ce7fb7bcdd36697b84000446d9204ece3325b5c
Nique1 <dominikasarata@gmail.com>
2023-12-13 15:25:31 +0100

    Refactoring in test class

--- src/test/java/package1/performanceMetrics/ResponseTimeMeasureTest.java
+++ src/test/java/package1/performanceMetrics/ResponseTimeMeasureTest.java
@@ -12,45 +12,58 @@ class ResponseTimeMeasureTest {
 
     @Test
     void testElapsedTimeWhenNotStarted(){
+        //given
+        //when
         ResponseTimeMeasure measure = new ResponseTimeMeasure();
+        //then
         assertThat(measure.getElapsedTime()).isEqualTo(-1);
     }
 
     @Test
     void testElapsedTimeWhenStartedButNotEnded(){
+        //given
         ResponseTimeMeasure measure = new ResponseTimeMeasure();
+        //when
         measure.startOperation();
+        //then
         assertThat(measure.getElapsedTime()).isEqualTo(-1);
     }
 
     @Test
     void testElapsedTimeWhenMeasurementCompleted(){
+        //given
         ResponseTimeMeasure measure = new ResponseTimeMeasure();
+        //when
         measure.startOperation();
-        //delay simulation
         try{
             Thread.sleep(1000);
         }catch (InterruptedException ex){
             ex.printStackTrace();
         }
         measure.endOperation();
+        //then
         assertThat(measure.getElapsedTime()).isGreaterThanOrEqualTo(1000);
     }
 
     @Test
     void testStartAndEndTime(){
+        //given
         ResponseTimeMeasure measure = new ResponseTimeMeasure();
+        //when
         Instant startTime = Instant.now();
         measure.startOperation();
         Instant endTime = Instant.now();
         measure.endOperation();
+        //then
         assertThat(measure.getStartTime()).isEqualTo(startTime);
         assertThat(measure.getEndTime()).isEqualTo(endTime);
     }
 
     @Test
     void testPrintMetrics(){
+        //given
         ResponseTimeMeasure measure = new ResponseTimeMeasure();
+        //when
         measure.startOperation();
         //delay simulation
         try{
@@ -67,6 +80,7 @@ class ResponseTimeMeasureTest {
         measure.printMetrics();
 
         String consoleOutput = outContent.toString();
+        //then
         assertThat(consoleOutput).contains("Start Time:");
         assertThat(consoleOutput).contains("End Time:");
         assertThat(consoleOutput).contains("Elapsed Time:");
